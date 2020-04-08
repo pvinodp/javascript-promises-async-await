b0fc35c23895d86c3f20a969d12d204ae804ad65
vinod prabhu <vinoprab@cisco.com>
2020-04-08 19:33:13 +0530

    updated code with 2 new mthods.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import  fetchWithTimeout from "./services.js";
+
+const movies = require('/data/movies.json');
+
 export function fetchMovies(){
-    
+
+    let resolveFunction = () => movies;
+     return fetchWithTimeout(1000)
 }
+
+moviePromise = fetchMovies();
+moviePromise.then((results) => {
+    console.log(results);
+});
