65eb0c630f268e6baba323ba63a9906ec9e43887
vinod prabhu <vinoprab@cisco.com>
2020-04-08 21:33:14 +0530

    updated code with 2 new mthods.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import { fetchWithTimeout } from './services';
-const movies = require('./data/movies.json');
-export function fetchMovies(){
+import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
 
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies() {
+
+    Promise.all([fetchMovies(), fetchBooks()])
+        .then(([books, movies]) => {
+            books,
+                movies
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
-const moviePromise = fetchMovies();
-moviePromise.then((results) => {
-    console.log(results);
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then((results) => {
+    console.log('getBooksAndMoviesPromise', results);
+});
+
+function getBooksOrMovies(){
+
+    return Promise.race( ([fetchMovies(), fetchBooks()])
+    .then (results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+}
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then( (results) => {
+
+    console.log('getBooksOrMoviesPromise', results);    
 });
