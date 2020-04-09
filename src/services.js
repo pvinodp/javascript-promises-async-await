64df0674036c6d6351269521291b5c44d420bf06
vinod prabhu <vinoprab@cisco.com>
2020-04-09 10:53:56 +0530

    updated code with 2 new mthods.

--- src/services.js
+++ src/services.js
@@ -1,22 +1,33 @@
-export function  fetchWithTimeout(delay){
+export function fetchWithTimeout(delay) {
 
-    return new Promise (resolve => setTimeout(resolve, delay));
+    return new Promise(resolve => setTimeout(resolve, delay));
 }
 
-export function fetchMovies(){
+export function fetchMovies() {
 
     return fetch("./data/movies.json")
-    .then((response) => response.json())
-    .then((movies) => movies )
-    .catch(error => {
-        console.log(error);
-    });    
+        .then((response) => response.json())
+        .then((movies) => movies)
+        .catch(error => {
+            console.log(error);
+        });
 }
 
-export function fetchBooks(){
+export function fetchBooks() {
 
     return fetch("./data/books.json")
-    .then( (response) =>  response.json())
-    .then ((books) => books)
-    .catch(error => console.log(error));
+        .then((response) => response.json())
+        .then((books) => books)
+        .catch(error => console.log(error));
+}
+
+export async function asyncFetchMovies() {
+
+    try {
+        const response = await fetch("/data/movies.json");
+        const results = await response.json();
+        return results;
+    } catch (error) {
+        console.log(error);
+    }
 }
